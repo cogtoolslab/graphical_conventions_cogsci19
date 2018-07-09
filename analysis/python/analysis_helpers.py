c45c87bc0be6c668b13b8be7de2a0ed31fe90d91
Judy Fan <judithfan@gmail.com>
2018-07-09 14:08:48 -0700

    add debug mode to get_complete_and_valid_games in  analysis_helpers

--- analysis/python/analysis_helpers.py
+++ analysis/python/analysis_helpers.py
@@ -14,13 +14,19 @@ colors = sns.color_palette("cubehelix", 5)
 ################### HELPERS FOR grpahical conventions analysis notebook ###########
 ###############################################################################################
 
-
-def get_complete_and_valid_games(games,coll,researchers=['A1MMCS8S8CTWKU']):
+def get_complete_and_valid_games(games,
+                                 coll,                                 
+                                 researchers=['A1MMCS8S8CTWKU'],
+                                 tolerate_undefined_worker=False):
     '''
-    Input: Accepts list of gameIDs that are in the database, and a list of the researcher worker ID's
+    Input: 
+        -- games: a list of gameIDs that are in the database, and a list of the researcher worker ID's
+        -- coll: the mongodb collection, e.g., db['3dObjects']['graphical_conventions']
+        -- researchers: the list of researchers, which we exclude from our analysis 
+        -- tolerate_undefined_worker: *only in debug mode*, we can allow games through that have undefined worker ID's
     Output: Returns list of complete and valid gameID, defined as: 
-        (1) complete games (correct number of trials==40)
-        (2) there were two real MTurk workers participating (i.e., not a researcher or undefined worker)
+        -- a complete game (correct number of trials==40)
+        -- there were two real MTurk workers participating (i.e., not a researcher or undefined worker)
 
     '''
     complete_games = []
@@ -37,6 +43,8 @@ def get_complete_and_valid_games(games,coll,researchers=['A1MMCS8S8CTWKU']):
             sketcher_check = (len(sketcher[0])>10) & len(sketcher)==1 ## length of workerID string should be long enough & there should be exactly one viewer    
             if (viewer_check) & (sketcher_check) & (not viewer_is_researcher) & (not sketcher_is_researcher):
                 real_workers = True
+            if tolerate_undefined_worker:
+                real_workers = True
         except:
             print 'There was something wrong with this game {}'.format(game)
 
@@ -45,6 +53,8 @@ def get_complete_and_valid_games(games,coll,researchers=['A1MMCS8S8CTWKU']):
         if num_clicks == 40:
             finished_game = True
 
+        ##print game, viewer_check, sketcher_check, viewer_is_researcher, sketcher_is_researcher, num_clicks
+            
         ## now if BOTH of the above conditions are true, bother to analyze them
         if (real_workers) & (finished_game):
             complete_games.append(game)
