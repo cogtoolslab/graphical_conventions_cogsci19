5650b072761510412e95ce5a2b3ef70277bf24d4
Judy Fan <judithfan@gmail.com>
2018-07-20 15:01:30 -0700

    set iterationName for set size 6 pilot to run2_chairs1k_size6

--- experiments/draw_chairs/game.core.js
+++ experiments/draw_chairs/game.core.js
@@ -27,7 +27,7 @@ var game_core = function(options){
   this.server = options.server ;
   this.projectName = '3dObjects';
   this.experimentName = 'graphical_conventions';
-  this.iterationName = 'testing'; // ['run0_bonusmeter','run1_chairsOnly','run2_chairs1k_setsize6']
+  this.iterationName = 'run2_chairs1k_size6'; // ['run0_bonusmeter','run1_chairsOnly','run2_chairs1k_size4','run2_chairs1k_size6']
   this.email = 'sketchloop@gmail.com';
   console.log("color randomized");
 
