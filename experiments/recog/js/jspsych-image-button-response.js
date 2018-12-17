e93d2d377edb411bde9f6aa78eba891dcfb231ea
Judy Fan <judithfan@gmail.com>
2018-12-17 16:53:37 -0700

    slightly less intense score bolding

--- experiments/recog/js/jspsych-image-button-response.js
+++ experiments/recog/js/jspsych-image-button-response.js
@@ -283,8 +283,8 @@ jsPsych.plugins["image-button-response"] = (function() {
       display_element.querySelector('#jspsych-image-button-response-button-' + target_index).style.border = "8px solid #66B03B"
       // also bold/enlarge the score in bottom left corner 
       display_element.querySelector('#score').innerHTML = 'bonus earned: ' + parseFloat(score).toFixed(3);
-      display_element.querySelector('#score').style.fontSize = '120%';      
-      display_element.querySelector('#score').style.fontWeight = 'bolder';
+      display_element.querySelector('#score').style.fontSize = '110%';      
+      display_element.querySelector('#score').style.fontWeight = 'bold';
     } else {
      // draw RED box around INCORRECT response and BLACK box around TARGET
      display_element.querySelector('#jspsych-image-button-response-button-' + target_index).style.border = "8px solid #282828"      
