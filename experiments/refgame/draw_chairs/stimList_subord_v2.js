59e8114bba10150097e628f1d76ef866028c483b
Judy Fan <judithfan@gmail.com>
2018-12-31 13:35:28 -0700

    add v2 stimList subord to suit needs of modified refgame 2.0

---
+++ experiments/refgame/draw_chairs/stimList_subord_v2.js
@@ -0,0 +1,50 @@
+// Shapenet chairs stimuli 
+// for Communication Task, Version 2.0 (refgame)
+// https://github.com/cogtoolslab/graphical_conventions/blob/master/experiments/README.md
+
+// December 31, 2018
+
+// What's new:
+// Added useSubmitButton flag in game.core.js
+// When set to true, the Sketcher must click the Submit button when they are done 
+// drawing for the Viewer to be able to see their drawing, so that the Viewer is not 
+// able to interrupt the Sketcher's drawing.
+// 30-second time limit in the speed bonus system still applies to the time taken 
+// since the Sketcher begins drawing until the Viewer selects an object
+// Minimize context variability & increase sampling density within context: 
+// The same sets of 4 dining and 4 waiting items are used across pairs. 
+// Assignment of repeated and control condition labels to each of these sets is 
+// randomized across pairs. Thus approx. half of pairs will see dining repeatedly 
+// (with waiting items as control), and half of pairs will see waiting repeatedly 
+// (with dining as control).
+
+// label 2 - dining
+var shapenet_30afd2ef2ed30238aa3d0a2f00b54836 = {filename: "30afd2ef2ed30238aa3d0a2f00b54836.png" , basic: "dining", subordinate: "dining_00" , subset:"A", cluster: 1, object: 0, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/30afd2ef2ed30238aa3d0a2f00b54836.png",width: 256, height: 256};
+var shapenet_30dc9d9cfbc01e19950c1f85d919ebc2 = {filename: "30dc9d9cfbc01e19950c1f85d919ebc2.png" , basic: "dining", subordinate: "dining_01" , subset:"A", cluster: 1, object: 1, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/30dc9d9cfbc01e19950c1f85d919ebc2.png",width: 256, height: 256};
+var shapenet_4c1777173111f2e380a88936375f2ef4 = {filename: "4c1777173111f2e380a88936375f2ef4.png" , basic: "dining", subordinate: "dining_02" , subset:"B", cluster: 1, object: 2, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/4c1777173111f2e380a88936375f2ef4.png",width: 256, height: 256};
+var shapenet_3466b6ecd040e252c215f685ba622927 = {filename: "3466b6ecd040e252c215f685ba622927.png" , basic: "dining", subordinate: "dining_03" , subset:"B", cluster: 1, object: 3, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/3466b6ecd040e252c215f685ba622927.png",width: 256, height: 256};
+var shapenet_38f87e02e850d3bd1d5ccc40b510e4bd = {filename: "38f87e02e850d3bd1d5ccc40b510e4bd.png" , basic: "dining", subordinate: "dining_04" , subset:"B", cluster: 1, object: 4, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/38f87e02e850d3bd1d5ccc40b510e4bd.png",width: 256, height: 256};
+var shapenet_3cf6db91f872d26c222659d33fd79709 = {filename: "3cf6db91f872d26c222659d33fd79709.png" , basic: "dining", subordinate: "dining_05" , subset:"B", cluster: 1, object: 5, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/3cf6db91f872d26c222659d33fd79709.png",width: 256, height: 256};
+var shapenet_3d7ebe5de86294b3f6bcd046624c43c9 = {filename: "3d7ebe5de86294b3f6bcd046624c43c9.png" , basic: "dining", subordinate: "dining_06" , subset:"A", cluster: 1, object: 6, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/3d7ebe5de86294b3f6bcd046624c43c9.png",width: 256, height: 256};
+var shapenet_56262eebe592b085d319c38340319ae4 = {filename: "56262eebe592b085d319c38340319ae4.png" , basic: "dining", subordinate: "dining_07" , subset:"A", cluster: 1, object: 7, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/56262eebe592b085d319c38340319ae4.png",width: 256, height: 256};
+// label 38 - waiting
+var shapenet_1d1641362ad5a34ac3bd24f986301745 = {filename: "1d1641362ad5a34ac3bd24f986301745.png" , basic: "waiting", subordinate: "waiting_00" , subset:"A", cluster: 3, object: 0, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/1d1641362ad5a34ac3bd24f986301745.png",width: 256, height: 256};
+var shapenet_1da9942b2ab7082b2ba1fdc12ecb5c9e = {filename: "1da9942b2ab7082b2ba1fdc12ecb5c9e.png" , basic: "waiting", subordinate: "waiting_01" , subset:"A", cluster: 3, object: 1, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/1da9942b2ab7082b2ba1fdc12ecb5c9e.png",width: 256, height: 256};
+var shapenet_2448d9aeda5bb9b0f4b6538438a0b930 = {filename: "2448d9aeda5bb9b0f4b6538438a0b930.png" , basic: "waiting", subordinate: "waiting_02" , subset:"B", cluster: 3, object: 2, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/2448d9aeda5bb9b0f4b6538438a0b930.png",width: 256, height: 256};
+var shapenet_23b0da45f23e5fb4f4b6538438a0b930 = {filename: "23b0da45f23e5fb4f4b6538438a0b930.png" , basic: "waiting", subordinate: "waiting_03" , subset:"B", cluster: 3, object: 3, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/23b0da45f23e5fb4f4b6538438a0b930.png",width: 256, height: 256};
+var shapenet_2b5953c986dd08f2f91663a74ccd2338 = {filename: "2b5953c986dd08f2f91663a74ccd2338.png" , basic: "waiting", subordinate: "waiting_04" , subset:"B", cluster: 3, object: 4, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/2b5953c986dd08f2f91663a74ccd2338.png",width: 256, height: 256};
+var shapenet_2e291f35746e94fa62762c7262e78952 = {filename: "2e291f35746e94fa62762c7262e78952.png" , basic: "waiting", subordinate: "waiting_05" , subset:"B", cluster: 3, object: 5, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/2e291f35746e94fa62762c7262e78952.png",width: 256, height: 256};
+var shapenet_2eaab78d6e4c4f2d7b0c85d2effc7e09 = {filename: "2eaab78d6e4c4f2d7b0c85d2effc7e09.png" , basic: "waiting", subordinate: "waiting_06" , subset:"A", cluster: 3, object: 6, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/2eaab78d6e4c4f2d7b0c85d2effc7e09.png",width: 256, height: 256};
+var shapenet_309674bdec2d24d7597976c675750537 = {filename: "309674bdec2d24d7597976c675750537.png" , basic: "waiting", subordinate: "waiting_07" , subset:"A", cluster: 3, object: 7, pose: 35, url: "https://s3.amazonaws.com/shapenet-graphical-conventions/309674bdec2d24d7597976c675750537.png",width: 256, height: 256};
+
+var stimList = [
+// label 2 - dining
+shapenet_30afd2ef2ed30238aa3d0a2f00b54836,shapenet_30dc9d9cfbc01e19950c1f85d919ebc2,shapenet_4c1777173111f2e380a88936375f2ef4,
+shapenet_3466b6ecd040e252c215f685ba622927,shapenet_38f87e02e850d3bd1d5ccc40b510e4bd,shapenet_3cf6db91f872d26c222659d33fd79709,
+shapenet_3d7ebe5de86294b3f6bcd046624c43c9,shapenet_56262eebe592b085d319c38340319ae4,
+// label 38 - waiting
+shapenet_1d1641362ad5a34ac3bd24f986301745,shapenet_1da9942b2ab7082b2ba1fdc12ecb5c9e,shapenet_2448d9aeda5bb9b0f4b6538438a0b930,
+shapenet_23b0da45f23e5fb4f4b6538438a0b930,shapenet_2b5953c986dd08f2f91663a74ccd2338,shapenet_2e291f35746e94fa62762c7262e78952,
+shapenet_2eaab78d6e4c4f2d7b0c85d2effc7e09,shapenet_309674bdec2d24d7597976c675750537
+];
+module.exports = stimList;
