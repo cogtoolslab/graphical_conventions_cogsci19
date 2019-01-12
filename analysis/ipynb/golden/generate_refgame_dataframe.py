a0fbad0173b4049e911336ef57ed9203ce853c1f
Judy Fan <judithfan@gmail.com>
2019-01-11 17:23:12 -0800

    update all master dataframes

--- analysis/ipynb/golden/generate_refgame_dataframe.py
+++ analysis/ipynb/golden/generate_refgame_dataframe.py
@@ -121,7 +121,7 @@ D = h.add_distractors_and_shapenet_ids(D)
 
 # write out main dataframe to results dir
 D = D.rename(index=str, columns={"Generalization": "generalization"})
-D.to_csv(os.path.join(results_dir, 'graphical_conventions.csv'))
+D.to_csv(os.path.join(results_dir, 'graphical_conventions.csv'), index=False)
 
 ## write out bis dataframe to results dir
 drawDuration_accuracy_bis, numStrokes_accuracy_bis = h.save_bis_scores(D, results_dir)
